3e9fe35f9cc65a0d35d58d2e7b55c689ab0e44e5
pambalos <bradleyjusticeca@gmail.com>
2018-09-14 13:57:02 -0700

    First commit
    base app up, layout.html copied from another project

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, url_for, flash, redirect, request
+from flask_wtf import FlaskForm
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "219nv8438vncjkxjfg9904jkcod4niv90"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+# Might be needed included only for development side, once launched can remove this as it should be https
+import os
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+@app.route('/')
+def check():
+    print("went into and running")
+
+if __name__ == '__main__':
+    app.run(debug=True)
