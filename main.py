b211b532cff08ed97b8db4c33df58232122bd378
pambalos <bradleyjusticeca@gmail.com>
2018-09-15 03:52:15 -0700

    Styling Idea
    Easy set up for quick integration

--- main.py
+++ main.py
@@ -2,16 +2,12 @@ from flask import Flask, render_template, url_for, flash, redirect, request, jso
 from flask_wtf import FlaskForm
 from flask_sqlalchemy import SQLAlchemy
 from math import sin, cos, sqrt, atan2, radians
-import requests, json
+import requests, json, csv
 from geopy.distance import geodesic
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "219nv8438vncjkxjfg9904jkcod4niv90"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
-# Might be needed included only for development side, once launched can remove this as it should be https
-import os
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 @app.route('/')
 @app.route('/check')
@@ -23,7 +19,7 @@ def check():
 def map():
     return render_template('map.html')
 
-@app.route('/map/api/<string:lat>/<string:long>', methods = ["GET"])
+@app.route('/map/<string:lat>/<string:long>', methods = ["GET"])
 def get_stations(lat, long):
     data = {}
     data['stations'] = []
@@ -47,10 +43,9 @@ def get_stations(lat, long):
         point2 = (float(station['LATITUDE']), float(station['LONGITUDE']))
         print(point1)
         distance = geodesic(point1, point2)
-        print("this station is: " + str(distance) + " away")
         if distance.km <= 5:
              stationlist['stations'].append(station)
-    print("long: " + str(long) + "     lat: " + str(lat))
+
     return jsonify(stationlist)
 
 if __name__ == '__main__':
